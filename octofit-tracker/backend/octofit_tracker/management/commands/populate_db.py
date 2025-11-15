0868eac716f0a4dd9704abe91e88da702094dd1b
rayudunsr <n.subbarayudu@accenture.com>
2025-11-15 02:23:31 +0000

    Add management command to populate octofit_db with test data

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,42 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Workout, Leaderboard
+from django.utils import timezone
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        # Clear existing data
+        Activity.objects.all().delete()
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Workout.objects.all().delete()
+        Leaderboard.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel', description='Marvel Superheroes')
+        dc = Team.objects.create(name='DC', description='DC Superheroes')
+
+        # Create users
+        users = [
+            User.objects.create(name='Spider-Man', email='spiderman@marvel.com', team=marvel),
+            User.objects.create(name='Iron Man', email='ironman@marvel.com', team=marvel),
+            User.objects.create(name='Captain America', email='cap@marvel.com', team=marvel),
+            User.objects.create(name='Batman', email='batman@dc.com', team=dc),
+            User.objects.create(name='Superman', email='superman@dc.com', team=dc),
+            User.objects.create(name='Wonder Woman', email='wonderwoman@dc.com', team=dc),
+        ]
+
+        # Create workouts
+        workout1 = Workout.objects.create(name='Cardio Blast', description='High intensity cardio', difficulty='Hard')
+        workout2 = Workout.objects.create(name='Strength Training', description='Build muscle', difficulty='Medium')
+
+        # Create activities
+        Activity.objects.create(user=users[0], type='Running', duration=30, calories=300, date=timezone.now())
+        Activity.objects.create(user=users[3], type='Cycling', duration=45, calories=400, date=timezone.now())
+
+        # Create leaderboard
+        Leaderboard.objects.create(team=marvel, points=1200)
+        Leaderboard.objects.create(team=dc, points=1100)
+
+        self.stdout.write(self.style.SUCCESS('Test data populated successfully!'))
